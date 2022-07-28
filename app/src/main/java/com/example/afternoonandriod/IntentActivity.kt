3cca6084fddbfc55b6a99ded716e587ba0eaccf0
festus-hub <kipchirchirfestus>
2022-07-28 16:17:58 +0300

    initial commit

--- app/src/main/java/com/example/afternoonandriod/IntentActivity.kt
+++ app/src/main/java/com/example/afternoonandriod/IntentActivity.kt
@@ -2,10 +2,16 @@ package com.example.afternoonandriod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IntentActivity : AppCompatActivity() {
+    lateinit var Buttonsms:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+        Buttonsms=findViewById(R.id.btn_sms)
+        Buttonsms.setOnClickListener {
+
+        }
     }
 }
--- app/src/main/java/com/example/afternoonandriod/WebActivity.kt
+++ app/src/main/java/com/example/afternoonandriod/WebActivity.kt
@@ -1,11 +1,34 @@
 package com.example.afternoonandriod
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class WebActivity : AppCompatActivity() {
+    lateinit var wb_view:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        wb_view=findViewById(R.id.btn_web)
+        WebView()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun WebView(){
+        wb_view.webViewClient= WebViewClient()
+        wb_view.apply {
+            loadUrl(  "https://facebook.com")
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (wb_view.canGoBack())wb_view.goBack()
+        super.onBackPressed()
     }
 }
